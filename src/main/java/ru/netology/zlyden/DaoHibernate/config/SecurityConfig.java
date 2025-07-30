c92866d92c82d16ddb0d13424fba1a6b4f64a5d7
Zlyden79 <zlyden79@gmail.com>
2025-07-30 15:27:14 +0500

    Реализовал аутентификацию согласно заданию - один endpoint доступен без аутентификации, остальные - с аутентификацией и даже авторизацией

---
+++ src/main/java/ru/netology/zlyden/DaoHibernate/config/SecurityConfig.java
@@ -0,0 +1,50 @@
+package ru.netology.zlyden.DaoHibernate.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        var uds = new InMemoryUserDetailsManager();
+
+        var u1 = User.withUsername("john")
+                .password(passwordEncoder().encode("12345"))
+                .authorities("search")
+                .build();
+
+        var u2 = User.withUsername("jane")
+                .password(passwordEncoder().encode("54321"))
+                .authorities("read")
+                .build();
+
+        uds.createUser(u1);
+        uds.createUser(u2);
+
+        return uds;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.formLogin(Customizer.withDefaults());
+        http.authorizeHttpRequests(c -> c.requestMatchers("/persons").permitAll()
+                .requestMatchers("/persons/by*").hasAuthority("search")
+                .anyRequest().authenticated());
+        return http.build();
+    }
+}
